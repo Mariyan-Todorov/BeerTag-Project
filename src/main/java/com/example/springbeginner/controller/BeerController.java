2ec48b8b0ee1c6981eba6323d81209277e35d206
Marian Todorov <mariyan.ts.todorov@gmail.com>
2026-06-05 15:36:06 +0300

    Separate project into business and persistence layers

--- src/main/java/com/example/springbeginner/controller/BeerController.java
+++ src/main/java/com/example/springbeginner/controller/BeerController.java
@@ -1,71 +1,68 @@
 package com.example.springbeginner.controller;
 
+import com.example.springbeginner.exceptions.DuplicateEntityException;
+import com.example.springbeginner.exceptions.EntityNotFoundException;
 import com.example.springbeginner.models.Beer;
+import com.example.springbeginner.services.BeerServiceImpl;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/beers")
 public class BeerController {
 
-    private final List<Beer> beers;
+    private BeerServiceImpl service;
 
     public BeerController(){
-        beers = new ArrayList<>();
-
-        beers.add(new Beer(1, "Glarus English Ale", 4.6));
-        beers.add(new Beer(2, "Rhombus Porter", 5.0));
+        this.service = new BeerServiceImpl();
     }
-
     @GetMapping
     public List<Beer> getAll(){
-        return beers;
+        return service.getAll();
     }
 
     @GetMapping("/{id}")
     public Beer getById(@PathVariable int id){
-        return beers.stream()
-                .filter(beer-> beer.getId() == id)
-                .findFirst()
-                .orElseThrow(()-> new ResponseStatusException(
-                        HttpStatus.NOT_FOUND,
-                        String.format("Beer with id %d not found.", id)));
+        try {
+            return service.getById(id);
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
     }
 
     @PostMapping
     public Beer create(@Valid @RequestBody Beer beer){
-        beers.add(beer);
+        try {
+            service.create(beer);
+        } catch (DuplicateEntityException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT,e.getMessage());
+        }
         return beer;
     }
 
-    @PutMapping
-    public Beer update(@RequestBody Beer beer){
-        Beer updateBeer = beers.stream()
-                .filter(item -> item.getId() == beer.getId())
-                .findFirst().orElseThrow(()-> new ResponseStatusException(
-                        HttpStatus.NOT_FOUND,
-                        String.format("Beer with id %id not found.", beer.getId())
-                ));
-        updateBeer.setAbv(beer.getAbv());
-        updateBeer.setName(beer.getName());
+    @PutMapping("/{id}")
+    public Beer update(@PathVariable int id, @RequestBody Beer beer){
+       try{
+           service.update(beer);
+       }catch (EntityNotFoundException e){
+           throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+       }catch (DuplicateEntityException e){
+           throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
+       }
 
-        return updateBeer;
+        return beer;
     }
 
     @DeleteMapping("/{id}")
     public void delete(@PathVariable int id){
-        Beer deleteBeer = beers.stream()
-                .filter(item -> item.getId() == id)
-                .findFirst().orElseThrow(()-> new ResponseStatusException(
-                        HttpStatus.NOT_FOUND,
-                        String.format("Beer with id %id no exist.", id)
-                ));
-
-        beers.remove(deleteBeer);
+        try{
+            service.delete(id);
+        }catch (EntityNotFoundException e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
     }
 }
---
+++ src/main/java/com/example/springbeginner/repositories/BeerRepositoryImpl.java
@@ -0,0 +1,52 @@
+package com.example.springbeginner.repositories;
+
+import com.example.springbeginner.exceptions.EntityNotFoundException;
+import com.example.springbeginner.models.Beer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BeerRepositoryImpl {
+
+    private List<Beer> beers;
+
+    public BeerRepositoryImpl(){
+        beers = new ArrayList<>();
+
+        beers.add(new Beer(1, "Glarus English Ale", 4.6));
+        beers.add(new Beer(2, "Rhombus Porter", 5.0));
+    }
+
+    public List<Beer> getAll(){
+        return beers;
+    }
+
+    public Beer getById(int id){
+        return beers.stream()
+                .filter(beer-> beer.getId() == id)
+                .findFirst()
+                .orElseThrow(()-> new EntityNotFoundException("Beer", id));
+    }
+
+    public Beer getByName(String name){
+        return beers.stream()
+                .filter(beer-> beer.getName().equals(name))
+                .findFirst()
+                .orElseThrow(()-> new EntityNotFoundException("Beer","name", name));
+    }
+
+    public void create(Beer beer){
+        beers.add(beer);
+    }
+
+    public void update(Beer beer){
+        Beer beerToUpdate = getById(beer.getId());
+        beerToUpdate.setName(beer.getName());
+        beerToUpdate.setAbv(beer.getAbv());
+    }
+
+    public void delete(int id){
+        Beer beerToDelete = getById(id);
+        beers.remove(beerToDelete);
+    }
+}
